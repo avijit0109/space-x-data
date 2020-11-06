fb17cef8cbe647f41899b099815caf3fd1069b05
avijit0109 <68628616+avijit0109@users.noreply.github.com>
2020-11-06 12:18:34 +0530

    Add files via upload

---
+++ demoFile.js
@@ -0,0 +1,18 @@
+let valString = 'eraser';
+
+// let arr = valString.split('');
+// console.log('{{{', arr);
+
+// for (let i = 0; i< arr.length; i++) {
+//     if(arr[i] === r) {
+//         if(arr[i+1] === a) {
+//             if (arr(i+2) === s) {
+//                 arr.splice(i, 3);
+//             }
+//         }
+//     }
+// }
+
+if (valString.includes('ras')) {
+    valString.split('ras');
+}
